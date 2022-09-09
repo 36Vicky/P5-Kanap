85cde9d781709b8b85e07d4a87d973dca2571b8e
23004J <mariavictoriagarciavilches85@gmail.com>
2022-09-09 20:22:44 +0200

    premier parti cart

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,3 +1,4 @@
+console.log("test")
 let localStorageProduct = JSON.parse(localStorage.getItem("product"));
 
 const getCartItem = document.getElementById('cart__items');
@@ -15,3 +16,34 @@ function addProductToCart() {
     let alreadyExists = userHashMap.hasOwnProperty(item.couleurDuProduit)
     return alreadyExists? false : userHashMap[item.couleurDuProduit] = 1
   })
+
+  for(i = 0; i < localStorageProduct.length; i++){
+  
+  totalFullCart += parseInt (localStorageProduct[i].nombreDeProduit,10)
+  
+  totalCart += localStorageProduct[i].nombreDeProduit*localStorageProduct[i].prixDuProduit
+  
+  fullCart = getCartItem.innerHTML = fullCart + `
+  <article class="cart__item" data-id=${localStorageProduct[i].idDuProduit} data-color="${localStorageProduct[i].couleurDuProduit}">
+              <div class="cart__item__img">
+                <img src="${localStorageProduct[i].imageDuProduit}" alt="${localStorageProduct[i].textAlternatif}">
+              </div>
+              <div class="cart__item__content">
+                <div class="cart__item__content__description">
+                  <h2>${localStorageProduct[i].nomDuProduit}</h2>
+                  <p>${localStorageProduct[i].descriptionDuProduit}</p>
+                  <p>${localStorageProduct[i].couleurDuProduit}</p>
+                  <p>${localStorageProduct[i].prixDuProduit} €</p>
+                </div>
+                <div class="cart__item__content__settings">
+                <div class="cart__item__content__settings__quantity">
+                  <p>Quantité</p>
+                  <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${localStorageProduct[i].nombreDeProduit}">
+                </div>
+                <div class="cart__item__content__settings__delete">
+                  <p class="deleteItem">Supprimer</p>
+                </div>
+              </div>
+            </div>
+          </article>
+          `;}        
